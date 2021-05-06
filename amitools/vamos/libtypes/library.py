3311493548ef4ccc930ea332eccac9ec57e0c38a
Christian Vogelgsang <chris@vogelgsang.org>
2021-05-06 22:58:36 +0200

    ported Library to AmigaClass

--- amitools/vamos/libtypes/library.py
+++ amitools/vamos/libtypes/library.py
@@ -1,134 +1,97 @@
-from amitools.vamos.libstructs import LibraryStruct
+from amitools.vamos.libstructs import LibraryStruct, NodeType
 from amitools.vamos.label import LabelLib
 from amitools.vamos.machine.opcodes import op_rts
-from amitools.vamos.atypes import (
-    AmigaType,
-    AmigaTypeDef,
-    BitFieldType,
-    BitField,
-    CString,
-)
-from .node import NodeType
+from amitools.vamos.astructs import AmigaClassDef
 
 
-@BitFieldType
-class LibFlags(BitField):
-    LIBF_SUMMING = 1 << 0
-    LIBF_CHANGED = 1 << 1
-    LIBF_SUMUSED = 1 << 2
-    LIBF_DELEXP = 1 << 3
+@AmigaClassDef
+class Library(LibraryStruct):
+    def __init__(self, mem, addr, **kwargs):
+        # if 'neg_size' is given then move base_addr of lib struct
+        neg_size = kwargs.get("neg_size")
+        if neg_size:
+            addr += neg_size
+        # now setup library struct
+        super().__init__(mem, addr, **kwargs)
+        print("addr", self.get_addr(), self._mem_obj)
+        # type?
+        type = kwargs.get("type")
+        if type:
+            self.node.type.val = type
+        # pri?
+        pri = kwargs.get("pri")
+        if pri:
+            self.node.setup.val = pri
+        # name?
+        name = kwargs.get("name")
+        if name:
+            self.node.name.setup(name, self._alloc, self._free_refs)
 
-
-@AmigaTypeDef(LibraryStruct, wrap={"flags": LibFlags})
-class Library(AmigaType):
-    def __init__(self, mem, addr, alloc=None):
-        AmigaType.__init__(self, mem, addr)
-        # extra alloc info
-        self._name_cstr = None
-        self._id_str_cstr = None
-        self._label = None
-        self._alloc = alloc
-        self._mem_obj = None
-
-    def set_name(self, val):
-        self.get_node().set_name(val)
-
-    def get_name(self, ptr=False):
-        return self.get_node().get_name(ptr)
-
-    def setup(self, version=0, revision=0, type=NodeType.NT_LIBRARY, pri=0, flags=0):
+    def new_lib(self, version=0, revision=0, type=NodeType.NT_LIBRARY, pri=0, flags=0):
         """set all lib values but name, id_str, pos_size, neg_size."""
-        node = self.get_node()
-        node.set_succ(0)
-        node.set_pred(0)
-        node.set_type(type)
-        node.set_pri(pri)
+        node = self.node
+        node.succ.aptr = 0
+        node.pred.aptr = 0
+        node.type.val = type
+        node.pri.val = pri
 
-        self.set_flags(flags)
-        self.set_pad(0)
-        self.set_version(version)
-        self.set_revision(revision)
-        self.set_sum(0)
-        self.set_open_cnt(0)
+        self.flags.val = flags
+        self.pad.val = 0
+        self.version.val = version
+        self.revision.val = revision
+        self.sum.val = 0
+        self.open_cnt.val = 0
 
     def fill_funcs(self, opcode=None, param=None):
         """quickly fill the function table of a library with an opcode and param"""
         if opcode is None:
             opcode = op_rts
-        neg_size = self.neg_size
+        neg_size = self.neg_size.val
+        base_addr = self.get_addr()
         off = 6
         while off < neg_size:
-            addr = self._addr - off
+            addr = base_addr - off
             self._mem.w16(addr, opcode)
             if param:
                 self._mem.w32(addr + 2, param)
             off += 6
 
     @classmethod
-    def alloc(cls, alloc, name, id_str, neg_size, pos_size=None, fd=None):
-        """alocate library and optional name and id_str CStrings"""
-        # calc size
-        if pos_size is None:
-            pos_size = cls.get_type_size()
-        # round neg_size to multiple of four
-        neg_size = (neg_size + 3) & ~3
-        total_size = pos_size + neg_size
-        # allocate lib
-        mem_obj = alloc.alloc_memory(None, total_size, add_label=False)
-        addr_base = mem_obj.addr + neg_size
-        lib = cls(alloc.get_mem(), addr_base)
-        lib._mem_obj = mem_obj
-        lib._alloc = alloc
-        lib._size = total_size
-        # set pos/neg size
-        lib.set_pos_size(pos_size)
-        lib.set_neg_size(neg_size)
-        # add label?
-        label_mgr = alloc.get_label_mgr()
-        if label_mgr:
-            struct = lib.get_type_struct()
-            lib._label = LabelLib(name, addr_base, neg_size, pos_size, struct, fd)
-            label_mgr.add_label(lib._label)
-        # set name and id_str
-        if name:
-            lib._name_cstr = CString.alloc(alloc, name)
-            lib.set_name(lib._name_cstr)
-        if id_str:
-            lib._id_str_cstr = CString.alloc(alloc, id_str)
-            lib.set_id_string(lib._id_str_cstr)
-        return lib
+    def _alloc(cls, alloc, tag, pos_size, neg_size, fd):
+        if tag is None:
+            tag = cls.get_signature()
+        return alloc.alloc_lib(
+            tag, cls, pos_size=pos_size, neg_size=neg_size, fd=fd
+        )
 
-    def free(self):
-        if not self._alloc:
-            raise RuntimeError("can't free")
-        mem_obj = self._mem_obj
-        if mem_obj is None:
-            addr = self._addr - self.neg_size
-            mem_obj = self._alloc.get_memory(addr)
-        self._alloc.free_memory(mem_obj)
-        # cleanup name
-        if self._name_cstr:
-            self._name_cstr.free()
-            self._name_cstr = None
-        # cleanup id str
-        if self._id_str_cstr:
-            self._id_str_cstr.free()
-            self._id_str_cstr = None
-        # cleanup label
-        if self._label:
-            self._alloc.get_label_mgr().remove_label(self._label)
-            self._label = None
-        # clear state
-        self._alloc = None
-        self._mem_obj = None
-        self._addr = 0
+    @classmethod
+    def _free(cls, alloc, mem_obj):
+        alloc.free_lib(mem_obj)
+
+    @classmethod
+    def alloc(cls, alloc, tag=None, pos_size=0, neg_size=0, fd=None, **kwargs):
+        if pos_size == 0:
+            pos_size = cls.get_byte_size()
+        # round size
+        neg_size = (neg_size + 3) & ~3
+        # alloc lib
+        return super().alloc(
+            alloc,
+            pos_size,
+            neg_size,
+            fd,
+            tag=tag,
+            pos_size=pos_size,
+            neg_size=neg_size,
+            **kwargs
+        )
 
     def calc_sum(self):
         """calc the lib sum and return it"""
-        neg_size = self.get_neg_size()
-        addr = self._addr - neg_size
+        neg_size = self.neg_size.val
+        addr = self.get_addr() - neg_size
         lib_sum = 0
-        while addr < self._addr:
+        while addr < self.get_addr():
             val = self._mem.r32(addr)
             lib_sum += val
             addr += 4
@@ -138,21 +101,17 @@ class Library(AmigaType):
     def update_sum(self):
         """calc new lib sum and store it"""
         lib_sum = self.calc_sum()
-        self.set_sum(lib_sum)
+        self.sum.val = lib_sum
         return lib_sum
 
     def check_sum(self):
         """calc and compare lib sum with stored value"""
         lib_sum = self.calc_sum()
-        got_sum = self.get_sum()
+        got_sum = self.sum.val
         return lib_sum == got_sum
 
     def inc_open_cnt(self):
-        cnt = self.get_open_cnt()
-        cnt += 1
-        self.set_open_cnt(cnt)
+        self.open_cnt.val += 1
 
     def dec_open_cnt(self):
-        cnt = self.get_open_cnt()
-        cnt -= 1
-        self.set_open_cnt(cnt)
+        self.open_cnt.val -= 1
--- test/unit/libtypes_library.py
+++ test/unit/libtypes_library.py
@@ -1,7 +1,7 @@
 import pytest
 from amitools.vamos.machine.opcodes import op_rts
-from amitools.vamos.libstructs import LibraryStruct
-from amitools.vamos.libtypes import Library, NodeType, LibFlags
+from amitools.vamos.libstructs import LibraryStruct, NodeType, LibFlags
+from amitools.vamos.libtypes import Library
 from amitools.vamos.label import LabelLib
 from amitools.fd import read_lib_fd
 
@@ -11,38 +11,37 @@ def libtypes_library_base_test(mem_alloc):
     # alloc lib
     name = "my.library"
     id_str = "my.library 1.2"
-    neg_size = 36
+    neg_size = 12
     pos_size = LibraryStruct.get_size()
-    lib = Library.alloc(alloc, name, id_str, neg_size)
-    assert lib.get_name() == name
-    assert lib.get_id_string() == id_str
-    size = lib.get_size()
-    assert pos_size == lib.get_pos_size()
-    assert neg_size == lib.get_neg_size()
-    assert size == pos_size + neg_size
+    lib = Library.alloc(alloc, name=name, id_string=id_str, neg_size=neg_size)
+    assert lib._mem_obj.size == neg_size + pos_size
+    assert pos_size == lib.pos_size.val
+    assert neg_size == lib.neg_size.val
+    assert lib.node.name.str == name
+    assert lib.id_string.str == id_str
     # lib setup
-    flags = LibFlags(LibFlags.LIBF_SUMMING, LibFlags.LIBF_CHANGED)
-    ltype = NodeType(NodeType.NT_DEVICE)
+    flags = LibFlags.LIBF_SUMMING | LibFlags.LIBF_CHANGED
+    ltype = NodeType.NT_DEVICE
     pri = -3
     ver = 1
     rev = 2
-    lib.setup(version=ver, revision=rev, pri=pri, flags=flags, type=ltype)
+    lib.new_lib(version=ver, revision=rev, pri=pri, flags=flags, type=ltype)
     # check lib
-    node = lib.get_node()
-    assert node.get_succ() is None
-    assert node.get_pred() is None
-    assert node.get_type() == ltype
-    assert node.get_pri() == pri
-    assert lib.get_flags() == flags
-    assert lib.get_pad() == 0
-    assert lib.get_neg_size() == neg_size
-    assert lib.get_pos_size() == pos_size
-    assert lib.get_version() == ver
-    assert lib.get_revision() == rev
-    assert lib.get_sum() == 0
-    assert lib.get_open_cnt() == 0
-    assert lib.get_name() == name
-    assert lib.get_id_string() == id_str
+    node = lib.node
+    assert node.succ.ref is None
+    assert node.pred.ref is None
+    assert node.type.val == ltype
+    assert node.pri.val == pri
+    assert lib.flags.val == flags
+    assert lib.pad.val == 0
+    assert lib.neg_size.val == neg_size
+    assert lib.pos_size.val == pos_size
+    assert lib.version.val == ver
+    assert lib.revision.val == rev
+    assert lib.sum.val == 0
+    assert lib.open_cnt.val == 0
+    assert lib.node.name.str == name
+    assert lib.id_string.str == id_str
     # fill funcs
     lib.fill_funcs()
     lib_base = lib.get_addr()
@@ -58,37 +57,36 @@ def libtypes_library_sum_test(mem_alloc):
     name = "my.library"
     id_str = "my.library 1.2"
     neg_size = 30
-    pos_size = LibraryStruct.get_size()
-    lib = Library.alloc(alloc, name, id_str, neg_size)
+    lib = Library.alloc(alloc, name=name, id_string=id_str, neg_size=neg_size)
     # assume rounded neg size
-    assert lib.get_neg_size() == 32
+    assert lib.neg_size.val == 32
     mem.w32(lib.addr - 32, 0xDEADBEEF)
     mem.w32(lib.addr - 28, 0xCAFEBABE)
     my_sum = (0xDEADBEEF + 0xCAFEBABE) & 0xFFFFFFFF
     lib_sum = lib.calc_sum()
     assert lib_sum == my_sum
     lib.update_sum()
-    assert lib.get_sum() == my_sum
+    assert lib.sum.val == my_sum
     assert lib.check_sum()
     # done
     lib.free()
     assert alloc.is_all_free()
 
 
-def libtypes_libary_open_cnt_test(mem_alloc):
+def libtypes_library_open_cnt_test(mem_alloc):
     mem, alloc = mem_alloc
     # alloc lib
     name = "my.library"
     id_str = "my.library 1.2"
     neg_size = 30
     pos_size = LibraryStruct.get_size()
-    lib = Library.alloc(alloc, name, id_str, neg_size)
+    lib = Library.alloc(alloc, name=name, id_string=id_str, neg_size=neg_size)
     # test open cnt
-    assert lib.get_open_cnt() == 0
+    assert lib.open_cnt.val == 0
     lib.inc_open_cnt()
-    assert lib.get_open_cnt() == 1
+    assert lib.open_cnt.val == 1
     lib.dec_open_cnt()
-    assert lib.get_open_cnt() == 0
+    assert lib.open_cnt.val == 0
     # done
     lib.free()
     assert alloc.is_all_free()
@@ -100,14 +98,15 @@ def libtypes_library_label_test(mem_alloc):
     id_str = "vamostest.library 0.1"
     fd = read_lib_fd("vamostest.library")
     neg_size = fd.get_neg_size()
-    lib = Library.alloc(alloc, name, id_str, neg_size, fd=fd)
+    lib = Library.alloc(alloc, name=name, id_string=id_str, neg_size=neg_size, fd=fd)
     # check for label
+    label = lib._mem_obj.label
     if alloc.get_label_mgr():
-        assert lib._label
-        assert isinstance(lib._label, LabelLib)
-        assert lib._label.fd == fd
+        assert label
+        assert isinstance(label, LabelLib)
+        assert label.fd == fd
     else:
-        assert not lib._label
+        assert not label
     # done
     lib.free()
     assert alloc.is_all_free()
