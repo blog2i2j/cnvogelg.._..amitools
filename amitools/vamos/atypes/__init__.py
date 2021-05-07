a2ac3f0d03ade04909b6079cff1af6a7e21c195f
Christian Vogelgsang <chris@vogelgsang.org>
2021-05-07 23:30:11 +0200

    removed atypes

--- amitools/vamos/atypes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .enum import EnumType, Enum
-from .bitfield import BitFieldType, BitField
-from .cstring import CString
-from .bstring import BString
-
-from .atype import AmigaType, AmigaTypeWithName
-from .atypedef import AmigaTypeDef
