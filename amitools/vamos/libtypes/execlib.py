b581a8a1a533e207f9a81c5ffaa64408ee0c2ef6
Christian Vogelgsang <chris@vogelgsang.org>
2021-05-08 22:29:44 +0200

    added missing execlib aliases

--- amitools/vamos/libtypes/execlib.py
+++ amitools/vamos/libtypes/execlib.py
@@ -5,6 +5,42 @@ from amitools.vamos.astructs import AmigaClassDef
 
 @AmigaClassDef
 class ExecLibrary(ExecLibraryStruct):
+    def __init__(self, mem, addr, **kwargs):
+        # if 'neg_size' is given then move base_addr of lib struct
+        neg_size = kwargs.get("neg_size")
+        if neg_size:
+            addr += neg_size
+        # now setup library struct
+        super().__init__(mem, addr, **kwargs)
+
+    @classmethod
+    def _alloc(cls, alloc, tag, pos_size, neg_size, fd):
+        if tag is None:
+            tag = cls.get_signature()
+        return alloc.alloc_lib(tag, cls, pos_size=pos_size, neg_size=neg_size, fd=fd)
+
+    @classmethod
+    def _free(cls, alloc, mem_obj):
+        alloc.free_lib(mem_obj)
+
+    @classmethod
+    def alloc(cls, alloc, tag=None, pos_size=0, neg_size=0, fd=None, **kwargs):
+        if pos_size == 0:
+            pos_size = cls.get_byte_size()
+        # round size
+        neg_size = (neg_size + 3) & ~3
+        # alloc lib
+        return super().alloc(
+            alloc,
+            pos_size,
+            neg_size,
+            fd,
+            tag=tag,
+            pos_size=pos_size,
+            neg_size=neg_size,
+            **kwargs
+        )
+
     def new_lib(self, version=0, revision=0, attn_flags=0, max_loc_mem=0):
         self.lib_node.new_lib(version, revision)
         self.attn_flags.val = attn_flags
